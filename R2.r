25dfd70518c9d15459001f7600d4e04d3a1caac5
christmas1409 <55200821+christmas1409@users.noreply.github.com>
2019-12-21 17:25:28 +0300

    Update R2.r

--- R2.r
+++ R2.r
@@ -1,6 +1,6 @@
 minimum <- function(m) {
   return(c(min(m), which(m == min(m), arr.ind = TRUE)))
-}
+} # which в данном примере - вернуть порядковые элементы m  равные минимальному значению m
 
 # prints
 print(minimum(matrix(c(3,5,7,1,2,5,7,3), nrow=2)))
